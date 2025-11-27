282d6e6fa01cd2c73974a291d822b1d734814589
williamdev20 <williamalvs45@gmail.com>
2025-11-27 10:50:30 -0300

    feat: Remover configurações de deploy

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn core.wsgi:application --workers 1 --bind 0.0.0.0:$PORT
--- core/settings.py
+++ core/settings.py
@@ -81,22 +81,13 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-if os.getenv("RAILWAY_ENVIRONMENT"):
-    # Production
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': '/railway/db.sqlite3',
-        }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-else:
-    # Local
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 
 # Password validation
